c7f06f564366039f02f32dbd35dfcca0df1d6313
Meshabaz <shabazameen1818@gmail.com>
2022-11-11 10:21:15 +0530

    components added

---
+++ src/components/experiance/Experiance.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./experiance.css";
+function Experiance() {
+  return <div>Experiance</div>;
+}
+
+export default Experiance;
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./nav.css";
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default Nav;
