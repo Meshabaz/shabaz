d17bf3928b49ca951420c80e3cd4a0f045d50b71
Meshabaz <shabazameen1818@gmail.com>
2022-11-11 16:21:36 +0530

    commited

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./header.css";
+import Attachments from "./Attachments";
+import me from "../../assets/me.jpg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Md Shabaz Ansari</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <Attachments />
+        <div className="me">
+          <img src={me} alt="" />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
